137cc480129b2f209ca0b4d67df810ab6e16598b
Denys Zaporozhets <dzaporozhets@pixsystem.com>
2019-11-11 19:41:33 +0200

    Changes to spirv reflection

---
+++ AdamantiumVulkan.SPIRV.Reflection/ShaderReflectionResource.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace AdamantiumVulkan.SPIRV.Reflection
+{
+    public class ShaderReflectionResource
+    {
+        private List<ShaderReflectionVariable> members;
+
+        public ShaderReflectionResource()
+        {
+            members = new List<ShaderReflectionVariable>();
+        }
+        
+        public ShaderReflectionVariable Description { get; internal set; }
+
+        public uint VariablesCount => (uint)members.Count;
+
+        public ShaderReflectionVariable GetVariable(uint index)
+        {
+            if (members.Count > index)
+            {
+                return members[(int)index];
+            }
+
+            return null;
+        }
+        
+        public void AddVariable(ShaderReflectionVariable variable)
+        {
+            if (!members.Contains(variable))
+            {
+                members.Add(variable);
+            }
+        }
+
+        public override string ToString()
+        {
+            return Description.Name;
+        }
+    }
+}
--- AdamantiumVulkan.SPIRV.Reflection/SpirvReflectionResult.cs
+++ AdamantiumVulkan.SPIRV.Reflection/SpirvReflectionResult.cs
@@ -7,23 +7,25 @@ namespace AdamantiumVulkan.SPIRV.Reflection
 {
     public class SpirvReflectionResult
     {
-        private List<ShaderReflectionVariable> resources; 
+        private List<ShaderReflectionResource> resources; 
 
         public SpirvReflectionResult()
         {
-            resources = new List<ShaderReflectionVariable>();
+            resources = new List<ShaderReflectionResource>();
         }
         
         public byte[] Bytecode { get; internal set; }
 
-        public ReadOnlyCollection<ShaderReflectionVariable> UniformBuffers => resources.Where(x=>x.Class == SpvcResourceType.UniformBuffer).ToList().AsReadOnly(); // cBuffer (constant buffers)
-        public ReadOnlyCollection<ShaderReflectionVariable> Samplers => resources.Where(x => x.Class == SpvcResourceType.SeparateSamplers).ToList().AsReadOnly(); // Samplers
-        public ReadOnlyCollection<ShaderReflectionVariable> Images => resources.Where(x => x.Class == SpvcResourceType.SeparateImage || x.Class == SpvcResourceType.SampledImage).ToList().AsReadOnly(); // Textures
-        public ReadOnlyCollection<ShaderReflectionVariable> StorageImages => resources.Where(x => x.Class == SpvcResourceType.StorageImage).ToList().AsReadOnly(); // RWTextures
-        public ReadOnlyCollection<ShaderReflectionVariable> AccelerationStructures => resources.Where(x => x.Class == SpvcResourceType.AccelerationStructure).ToList().AsReadOnly(); // RWBuffers
-        public ReadOnlyCollection<ShaderReflectionVariable> StorageBuffers => resources.Where(x => x.Class == SpvcResourceType.StorageBuffer).ToList().AsReadOnly(); // StructuredBuffers
+        public ReadOnlyCollection<ShaderReflectionResource> UniformBuffers => resources.Where(x=>x.Description.Class == SpvcResourceType.UniformBuffer).ToList().AsReadOnly(); // cBuffer (constant buffers)
+        public ReadOnlyCollection<ShaderReflectionResource> Samplers => resources.Where(x => x.Description.Class == SpvcResourceType.SeparateSamplers).ToList().AsReadOnly(); // Samplers
+        public ReadOnlyCollection<ShaderReflectionResource> Images => resources.Where(x => x.Description.Class == SpvcResourceType.SeparateImage || x.Description.Class == SpvcResourceType.SampledImage).ToList().AsReadOnly(); // Textures
+        public ReadOnlyCollection<ShaderReflectionResource> StorageImages => resources.Where(x => x.Description.Class == SpvcResourceType.StorageImage).ToList().AsReadOnly(); // RWTextures
+        public ReadOnlyCollection<ShaderReflectionResource> AccelerationStructures => resources.Where(x => x.Description.Class == SpvcResourceType.AccelerationStructure).ToList().AsReadOnly(); // RWBuffers
+        public ReadOnlyCollection<ShaderReflectionResource> StorageBuffers => resources.Where(x => x.Description.Class == SpvcResourceType.StorageBuffer).ToList().AsReadOnly(); // StructuredBuffers
 
-        internal void AddShaderResource(ShaderReflectionVariable resource)
+        public ReadOnlyCollection<ShaderReflectionResource> AllResources => resources.AsReadOnly();
+
+        internal void AddShaderResource(ShaderReflectionResource resource)
         {
             if (!resources.Contains(resource))
             {
