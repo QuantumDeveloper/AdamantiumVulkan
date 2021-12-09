66b841a20688df6984e379f70e5231ace0c217d0
black_joker <mderkins@gmail.com>
2021-12-09 22:34:00 +0200

    Regenerate bindings with performance improvements argument

--- AdamantiumVulkan.Shaders/Generated/Interop/AdamantiumVulkan.Shaders.MarshalDelegates.cs
+++ AdamantiumVulkan.Shaders/Generated/Interop/AdamantiumVulkan.Shaders.MarshalDelegates.cs
@@ -14,11 +14,13 @@ namespace AdamantiumVulkan.Shaders.Interop
     ///<summary>
     /// An includer callback type for mapping an #include request to an include result. The user_data parameter specifies the client context. The requested_source parameter specifies the name of the source being requested. The type parameter specifies the kind of inclusion request being made. The requesting_source parameter specifies the name of the source containing the #include request. The includer owns the result object and its contents, and both must remain valid until the release callback is called on the result object.
     ///</summary>
+    [SuppressUnmanagedCodeSecurity]
     [UnmanagedFunctionPointer(CallingConvention.Winapi)]
     public delegate System.IntPtr ShadercIncludeResolveFn([In, Out] System.IntPtr user_data, [In] [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(ConstCharPtrMarshaler))] string requested_source, [In] int type, [In] [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(ConstCharPtrMarshaler))] string requesting_source, [In] ulong include_depth);
     ///<summary>
     /// An includer callback type for destroying an include result.
     ///</summary>
+    [SuppressUnmanagedCodeSecurity]
     [UnmanagedFunctionPointer(CallingConvention.Winapi)]
     public delegate void ShadercIncludeResultReleaseFn([In, Out] System.IntPtr user_data, [In] AdamantiumVulkan.Shaders.Interop.ShadercIncludeResult include_result);
 
